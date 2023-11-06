f6876f25af9c0fdfa0b0e1bf212da2298393dbb8
Rafi <delwar2021bd@gmail.com>
2023-11-06 10:17:02 +0600

    added find post and query search for foods

--- index.js
+++ index.js
@@ -1,20 +1,77 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 const cors = require('cors');
-const app = express()
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
 
 // middleware below
 app.use(cors({
     credentials: true,
-    origin: ['']
+    origin: ['http://localhost:5173']
 }));
 app.use(express.json());
 
 // ----------------------
 
 
+// ----------------------
 // mongodb connection
+// ----------------------
+
+
+// 1. connecting to the db
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irfnbkn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+// 2. running the func
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const foodCollection = client.db("shareFood").collection("foods");
+    
+
+    app.get('/foods', async(req, res) => {
+        const query = req.query;
+        const sort = parseInt(query.sort) ;
+        console.log(sort);
+        const result = await foodCollection.find().sort({date: sort}).toArray();
+        console.log(result)
+        res.send(result);
+    })
+
+    app.post('/foods', async(req, res) =>{
+        const foodDetails = req.body;
+        console.log('insert req: ', foodDetails);
+        const result = await foodCollection.insertOne(foodDetails);
+        res.send({success: true, result});
+    })
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
